4211045c0061f779c9dd55621616a49a1ae0c115
GitHub Action <action@github.com>
2025-03-06 23:26:04 +0000

    Initialize template repository proper
    Set package name to com.github.butterfriez.muted
    Set project name to muted
    Set modid to muted

--- src/main/java/com/github/butterfriez/muted/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/butterfriez/muted/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.butterfriez.muted.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
